e188d1f261c179800b3e9eb97badf625c9494011
NetologyyTeest <135550062+NetologyyTeest@users.noreply.github.com>
2024-03-15 16:57:41 +0300

    change commit

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True, unique=True)
+    name = models.CharField(max_length = 30)
+    image = models.TextField()
+    price = models.IntegerField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default="True")
+    slug = models.SlugField(blank=True, unique=True, verbose_name='URL')
+
